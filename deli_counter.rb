3c691dbfdd7ee73092ff92ed6658c0de249bd554
Luke Glayat <glayatle@g.cofc.edu>
2017-07-08 03:18:15 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,40 @@
 # Write your code here.
+
+def line(arr)
+
+  if arr.length == 0
+    puts "The line is currently empty."
+    return
+  end
+
+  phrase = "The line is currently: "
+  phrase2 = ""
+  counter = 1
+
+  arr.each do |name|
+    phrase2 += "#{counter}. #{name} "
+    counter += 1
+  end
+
+  phrase = phrase + phrase2
+
+
+  puts phrase[0..-2]
+end
+
+def now_serving(arr)
+  if arr.length == 0
+    puts "There is nobody waiting to be served!"
+    return
+  end
+
+  name = arr.shift
+  puts "Currently serving #{name}."
+end
+
+def take_a_number(arr, name)
+
+  arr.push(name)
+
+  puts "Welcome, #{name}. You are number #{arr.length} in line."
+end
